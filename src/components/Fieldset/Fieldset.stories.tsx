926bd5f6084303358a5763044ff8ed4859469445
mynamesleon <contact@mynamesleon.com>
2024-08-24 17:05:03 +0100

    update Fieldset story with better example

--- src/components/Fieldset/Fieldset.stories.tsx
+++ src/components/Fieldset/Fieldset.stories.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import Fieldset from './Fieldset';
 import { FieldsetProps } from './Fieldset.types';
+import Input from '../Input';
+import EasyField from '../EasyField';
+import EasyForm from '../EasyForm';
+
+const children = (
+  <>
+    <EasyField name="name" type="text" label="Name" component={Input} />
+    <EasyField name="email" type="email" label="Email" component={Input} />
+    <EasyField
+      name="telephone"
+      type="tel"
+      label="Contact number"
+      component={Input}
+    />
+  </>
+);
 
 export default {
   title: 'Components/Fieldset',
   component: Fieldset,
 };
 
-export const StandardUsage = (props: FieldsetProps) => <Fieldset {...props} />;
+export const StandardUsage = (props: FieldsetProps) => (
+  <EasyForm>
+    <Fieldset {...props} />
+  </EasyForm>
+);
+
 StandardUsage.args = {
-  legend: 'Legend text',
-  children: 'Children go here',
+  legend: 'Contact details',
+  children,
 };
 
 export const VisuallyHiddenLegend = StandardUsage.bind({});
 VisuallyHiddenLegend.args = {
   visuallyHiddenLegend: true,
-  children: 'Children go here',
-  label: 'Legend text',
+  label: 'Contact details',
+  children,
 };
